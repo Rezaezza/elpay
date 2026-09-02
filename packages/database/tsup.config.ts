c191ff50bfef4fa7b487bad5570a211ea01167f0
Rezaezza <ezzasususgm@gmail.com>
2026-09-02 20:13:30 +0700

    fix: generate prisma client before database build

--- packages/database/tsup.config.ts
+++ packages/database/tsup.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
+  dts: true,
   clean: true,
   sourcemap: true,
   target: "es2022",
-
-  // sementara matikan pembuatan declaration
-  dts: false,
 });
